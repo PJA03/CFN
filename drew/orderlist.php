d9e3904d2a0f554a6028a6fbc825e80ed45d20fa
langggam <jatm0819@gmail.com>
2025-03-25 10:33:53 +0800

    orderlist minor fix header

--- drew/orderlist.php
+++ drew/orderlist.php
@@ -49,7 +49,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["orderID"])) {
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="stylesheet" href="orderlist.css">
+    <link rel="stylesheet" href="orderlist.css?v=<?php echo time(); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Order List</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
