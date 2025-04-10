fc3f51518fc6711b9ac2cd873a118e35f131ba7c
httpgarcia <kristinagarciaustjhs@gmail.com>
2025-04-11 02:02:41 +0800

    underline cart

--- drew/cart.php
+++ drew/cart.php
@@ -398,7 +398,7 @@ if (isset($_POST['cancel_cart'])) {
         <p class="usernamedisplay">Bonjour, <?php echo htmlspecialchars($user['username'], ENT_QUOTES, 'UTF-8'); ?>!</p>            
         <div class="icons">
             <a href = "../Home_Page/home.php"><i class="fa-solid fa-house home"></i></a>
-            <a href ="../drew/cart.php"><i class="fa-solid fa-cart-shopping cart"></i></a>
+            <a href ="../drew/cart.php"><i class="fa-solid fa-cart-shopping cart active"></i></a>
             <a href="../User_Profile_Page/UserProfile.php"><i class="far fa-user-circle fa-2x icon-profile"></i></a>
         </div>
     </div>
@@ -406,7 +406,6 @@ if (isset($_POST['cancel_cart'])) {
 
 
 <main>
-    <h1 class="cart-title">Cart</h1>
     <section class="cart-container">
         <div class="cart-content">
             <table class="cart-table">
